09162b746bb2204a2aeb1e22a53e7371a37e145d
Your Name <you@example.com>
2018-12-24 19:31:54 +0530

    Updates

---
+++ precache-manifest.9f35090fead6ebcdb0876572507e74c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a34560c2a30924fff41004c9c6ee2014",
+    "url": "/profile/static/media/profile.a34560c2.jpg"
+  },
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "acffecf21c92b4d4f3d4",
+    "url": "/profile/static/js/main.acffecf2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/profile/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "acffecf21c92b4d4f3d4",
+    "url": "/profile/static/css/main.ff9bf5aa.chunk.css"
+  },
+  {
+    "revision": "057cdc1210583a3532aee8a9ba9a149a",
+    "url": "/profile/index.html"
+  }
+];
